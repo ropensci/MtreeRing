447b937b71a71ff0fbfe7a04e74029c390fe080a
Jingning Shi <31177746+JingningShi@users.noreply.github.com>
2019-03-02 14:18:52 +0800

    Add files via upload

--- inst/doc/introduction.R
+++ inst/doc/introduction.R
@@ -1,14 +1,30 @@
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages("MtreeRing")
+
+## ----eval=FALSE----------------------------------------------------------
+#  # install.packages("devtools")
+#  devtools::install_github("JingningShi/MtreeRing")
+
+## ------------------------------------------------------------------------
+library(MtreeRing)
 ## Read and plot a tree ring image
 img.name <- system.file("001.png", package = "MtreeRing")
 t1 <- imgInput(img = img.name, dpi = 1200)
 
+## ------------------------------------------------------------------------
 ## Split a long core sample into 2 pieces to
 ## get better display performance and use the
 ## watershed algorithm to detect ring borders:
 t2 <- autoDetect(ring.data = t1, auto.path = TRUE, seg = 2, method = 'watershed')
 
+## ------------------------------------------------------------------------
 img.name <- system.file("incline.png", package = "MtreeRing")
 t1 <- imgInput(img = img.name, dpi = 1200)
 t2 <- autoDetect(ring.data = t1, incline = TRUE, method = 'watershed')
 
+## ------------------------------------------------------------------------
 rw.df <- calcRingWidth(ring.data = t2, seriesID = "940220")
+
+## ----eval=FALSE----------------------------------------------------------
+#  launchMtRApp()
+
