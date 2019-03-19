f38726243c4c573b1b71ec74f27e521c4ed52a0f
Jingning Shi <31177746+JingningShi@users.noreply.github.com>
2019-03-19 15:30:38 +0800

    Add files via upload

--- tests/testthat/test-launchMtRApp.R
+++ tests/testthat/test-launchMtRApp.R
@@ -1,9 +1,9 @@
-context("imgInput")
+context("launchMtRApp")
 
 test_that("the function run a app", {
   
   a <- launchMtRApp()
-  
+  a
   expect_is(a, "shiny.appobj")
   expect_type(a, "list")
   
