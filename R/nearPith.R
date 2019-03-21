e7eab063d8db40e025eea876389d88bb0336a5fc
Jingning Shi <31177746+JingningShi@users.noreply.github.com>
2019-03-21 15:53:42 +0800

    Add files via upload

--- R/nearPith.R
+++ R/nearPith.R
@@ -82,54 +82,9 @@ nearPith <- function(ring.data, inner.arc = TRUE, last.yr = NULL,
   dp <- x.dpi / 25.4 
   dev.set(device.number)
   dimt <- attributes(ring.data)$dimt
-  text.line <- 4
-  step.number <- 1
-  if (inner.arc) {
-    mtext(paste0('Step ', step.number, ': Click the left ',
-                 'mouse button to add a horizontal path.'), 
-          side = 1, line = text.line, adj = 0)
-    step1 <- locator(n = 1, type = "n")
-    py <- round(step1$y) 
-    abline(h = py, lty = 2, lwd = 2, col = color)
-    text.line <- 2 + text.line
-    mtext(paste0("Step ", step.number, " is already done."), side = 1, 
-          line = text.line, adj = 0, col = "blue")
-    text.line <- 2 + text.line
-    step.number <- 1 + step.number
-    mtext(paste0('Step ', step.number, ' : Click the left mouse button',
-                 ' to add three points on the arc you select.'), 
-          side = 1, line = text.line, adj = 0)
-    arc.a <- locator(1, type = "n")
-    points(arc.a$x, py, pch = border.type, col = color, cex = label.cex)
-    arc.b <- locator(1, type = "n")
-    points(arc.b$x, py, pch = border.type, col = color, cex = label.cex)
-    ab.x <- (arc.a$x + arc.b$x)/2
-    abline(v = ab.x, lwd = 1, lty = 2, col = color)
-    arc.c <- locator(1, type = "n")
-    points(ab.x, arc.c$y, pch = border.type, col = color, cex = label.cex)
-    text.line <- 2 + text.line
-    mtext(paste0("Step ", step.number, " is already done "), side = 1, 
-          line = text.line, adj = 0, col = "blue")
-  }  
-  if (!inner.arc) {
-    mtext(paste0('Step ', step.number, ' :Click the left mouse button to add ', 
-                 'two points. A straight line passing through ',
-                 'these two points will be added to the image.'), 
-          side = 1, line = text.line, adj = 0)
-    p1 <- locator(1, type = 'p', pch = 19)
-    points(p1$x, p1$y, pch = border.type, col = color, cex = label.cex)
-    p2 <- locator(1, type = 'p', pch = 19)
-    points(p2$x, p2$y, pch = border.type, col = color, cex = label.cex)
-    path.x <- c(p1$x, p2$x)
-    path.y <- c(p1$y, p2$y)
-    path.xy <- lm(path.y ~ path.x + 1)
-    a <- coef(path.xy)[1]
-    b <- coef(path.xy)[2]
-    abline(a = a, b = b, lwd = 2, lty = 2, col = color)
-    text.line <- 2 + text.line
-    mtext(paste0('Step ', step.number, ' is already done.'), side = 1, 
-          line = text.line, adj = 0, col = 'blue')
-  }  
+  pos <- add_path(inner.arc, border.type, color, label.cex)
+  text.line <- pos$t
+  step.number <- pos$s
   text.line <- 2 + text.line
   mtext(text = 'Please mark ring boundaries along the user-defined path', 
         side = 1, line = text.line, adj = 0)
@@ -138,6 +93,10 @@ nearPith <- function(ring.data, inner.arc = TRUE, last.yr = NULL,
   bor.x <- sort(bor.xy$x)
   bor.y <- bor.xy$y[order.x]
   if (inner.arc) {
+    arc.a <- pos$arc$a
+    arc.b <- pos$arc$b
+    arc.c <- pos$arc$c
+    py <- pos$p
     bor.x <- c(bor.x, min(arc.a$x, arc.b$x))
     bor.y <- c(bor.y, py)
   }
@@ -151,12 +110,7 @@ nearPith <- function(ring.data, inner.arc = TRUE, last.yr = NULL,
       text(bor.x, bor.y, yr.list, adj = c(1.25, 0.25), 
            col = color, cex = label.cex, srt = 90)
   }
-  bor.distance <- function(bor.x, bor.y) {
-    diff.x <- diff(bor.x)
-    diff.y <- diff(bor.y)
-    dis.pith <- sqrt(diff.x^2 + diff.y^2)
-    return(dis.pith)
-  }
+
   if (inner.arc) {
     l <- abs(arc.a$x - arc.b$x)
     h <- abs(arc.c$y - py)
@@ -186,4 +140,75 @@ nearPith <- function(ring.data, inner.arc = TRUE, last.yr = NULL,
     }
   }
   return(df.pith)
+}
+
+
+add_path <- function(inner.arc, border.type, color, label.cex) {
+  coordinate <- list()
+  text.line <- 4
+  step.number <- 1
+  if (inner.arc) {
+    mtext(paste0('Step ', step.number, ': Click the left ',
+      'mouse button to add a horizontal path.'), 
+      side = 1, line = text.line, adj = 0)
+    step1 <- locator(n = 1, type = "n")
+    py <- round(step1$y) 
+    abline(h = py, lty = 2, lwd = 2, col = color)
+    text.line <- 2 + text.line
+    mtext(paste0("Step ", step.number, " is already done."), side = 1, 
+      line = text.line, adj = 0, col = "blue")
+    text.line <- 2 + text.line
+    step.number <- 1 + step.number
+    mtext(paste0('Step ', step.number, ' : Click the left mouse button',
+      ' to add three points on the arc you select.'), 
+      side = 1, line = text.line, adj = 0)
+    ##arc
+    arc.position <- mark_arc(py, border.type, color, label.cex)
+    coordinate <- c(coordinate, list(arc = arc.position, p = py))
+    text.line <- 2 + text.line
+    mtext(paste0("Step ", step.number, " is already done "), side = 1, 
+      line = text.line, adj = 0, col = "blue")
+  }  
+  if (!inner.arc) {
+    mtext(paste0('Step ', step.number, ' :Click the left mouse button to add ', 
+      'two points. A straight line passing through ',
+      'these two points will be added to the image.'), 
+      side = 1, line = text.line, adj = 0)
+    path.position <- inclined_path(border.type, color, label.cex)
+    path.xy <- lm(path.position$y ~ path.position$x + 1)
+    a <- coef(path.xy)[1]
+    b <- coef(path.xy)[2]
+    abline(a = a, b = b, lwd = 2, lty = 2, col = color)
+    text.line <- 2 + text.line
+    mtext(paste0('Step ', step.number, ' is already done.'), side = 1, 
+      line = text.line, adj = 0, col = 'blue')
+  }  
+  coordinate <- c(coordinate, list(t = text.line, s = step.number))
+}
+
+mark_arc <- function(py, border.type, color, label.cex) {
+  arc.a <- locator(1, type = "n")
+  points(arc.a$x, py, pch = border.type, col = color, cex = label.cex)
+  arc.b <- locator(1, type = "n")
+  points(arc.b$x, py, pch = border.type, col = color, cex = label.cex)
+  ab.x <- (arc.a$x + arc.b$x)/2
+  abline(v = ab.x, lwd = 1, lty = 2, col = color)
+  arc.c <- locator(1, type = "n")
+  points(ab.x, arc.c$y, pch = border.type, col = color, cex = label.cex)
+  list(a = arc.a, b = arc.b, c = arc.c)
+}
+
+inclined_path <- function(border.type, color, label.cex) {
+  p1 <- locator(1, type = 'p', pch = 19)
+  points(p1$x, p1$y, pch = border.type, col = color, cex = label.cex)
+  p2 <- locator(1, type = 'p', pch = 19)
+  points(p2$x, p2$y, pch = border.type, col = color, cex = label.cex)
+  list(x = c(p1$x, p2$x), y = c(p1$y, p2$y))
+}
+
+bor.distance <- function(bor.x, bor.y) {
+  diff.x <- diff(bor.x)
+  diff.y <- diff(bor.y)
+  dis.pith <- sqrt(diff.x^2 + diff.y^2)
+  return(dis.pith)
 }
