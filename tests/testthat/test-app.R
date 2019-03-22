8e9e520158302e63d3816ab8019bf84fa8006330
Jingning Shi <31177746+JingningShi@users.noreply.github.com>
2019-03-22 17:33:35 +0800

    Delete test-app.R

--- tests/testthat/test-app.R
+++
@@ -1,8 +0,0 @@
-context('test app')
-
-test_that("app works", {
-  library(shinytest)
-  skip_on_cran()
-  app_dir <- system.file('mtr_app/', package = 'MtreeRing')
-  expect_pass(testApp(app_dir, compareImages = FALSE))
-})
